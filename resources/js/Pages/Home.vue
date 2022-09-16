a9c5fdbf935fe5c531d390dee771101b7b574dcf
Vladislav Osmianskij <vlaosm@gmail.com>
2022-09-16 18:30:42 +0300

    Installing and configuring Vue3 and Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head>
+        <title>Hello, world!</title>
+    </Head>
+    <main class="fixed inset-0 grid place-items-center">
+        <h1 class="text-2xl">Hello, world!</h1>
+    </main>
+</template>
